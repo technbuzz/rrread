6a02011db60f3f0f8c790ec4fa87103c5827e5b2
Sami Ullah Khan <samiullah.khan.1989@gmail.com>
2021-05-05 10:25:36 +0400

    add firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBAi2-CXuLL3coedJPdL4mzeK4I0B2YHbs",
+    authDomain: "rrread-c1f1d.firebaseapp.com",
+    projectId: "rrread-c1f1d",
+    storageBucket: "rrread-c1f1d.appspot.com",
+    messagingSenderId: "651495333010",
+    appId: "1:651495333010:web:6a53236cfebbf13efcda4b",
+    measurementId: "G-HWZ12CLS9Q"
+  }
 };
