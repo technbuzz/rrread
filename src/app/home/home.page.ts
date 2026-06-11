86db11c5efeb98842b21d9bfa17686584141e05c
technbuzz <samiullah.khan.1989@gmail.com>
2026-06-11 13:54:34 +0400

    WIP: :construction: moving angular to signals

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,16 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { addIcons } from 'ionicons';
 import { add } from 'ionicons/icons';
 
+import { RouterLink } from '@angular/router';
 import { IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonTitle, IonToolbar } from "@ionic/angular/standalone";
 import { FirebaseDatePipe } from '../shared/fbDate.pipe';
 import { BookListComponent } from './book-list/book-list.component';
-import { LoadingBookComponent } from './loading-book/loading-book.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  imports: [ FirebaseDatePipe, RouterLink, LoadingBookComponent, BookListComponent, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon],
+  imports: [ FirebaseDatePipe, RouterLink,  BookListComponent, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon],
   standalone: true,
   styleUrls: ['home.page.scss'],
 })
