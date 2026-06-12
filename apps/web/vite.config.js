3c70929098855c5b42faa33346c98cb5a1b2f5c3
technbuzz <samiullah.khan.1989@gmail.com>
2026-06-12 17:59:43 +0400

    feat[react]: :sparkles: added router
    We have added @transtack/react-router config, with basic route, I
    haven't realized all along that I am using the file based routing I was
    keep checking the code based routing

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
+    tanstackRouter(),
     tailwindcss(),
     react()],
 })
