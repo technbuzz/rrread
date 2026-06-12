b0f9ddc6cbef7bfde0b504521c8a124ff6529db2
technbuzz <samiullah.khan.1989@gmail.com>
2026-06-12 14:25:02 +0400

    feat: :sparkles: added book listing

--- apps/web/src/BookItem.jsx
+++ apps/web/src/BookItem.jsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from "react"
+
+const intl = Intl.DateTimeFormat("en-US")
+
 export function BookItem() {
+  const [books, setBooks] = useState([])
+
+  async function fetchBooks() {
+    const response = await fetch("https://firestore.googleapis.com/v1/projects/rrread-c1f1d/databases/(default)/documents/readings") 
+    const data = await response.json() 
+    setBooks(data.documents)
+    console.log({books})
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
   return (
     <>
-      <div className="flex">
-        <img />
-        <div className="border-b">
-          <h3 className="font-bold">Title</h3>
-          <p className="text-gray-500">This is description</p>
-          <p className="text-gray-500">1/1/2022</p>
-        </div>
-      </div>
+      {
+        books.map((book, index) => (
+          <div key={index} className="flex">
+            <div className="border-b">
+              <h3 className="font-semibold">{book.fields.title.stringValue}</h3>
+              <small className="text-gray-500">{book.fields.description.stringValue}</small>
+              <small className="text-gray-500">{ intl.format(new Date(book.fields.date.timestampValue))}</small>
+            </div>
+          </div>
+        ))
+        
+      }
     </>
 
   )
 }
+
+
+            // <img src={book.fields.img.stringValue} />
